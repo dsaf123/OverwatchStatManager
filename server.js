1df09f29dc2b57ca9ba7ef8c03dc27c40ecf92f7
Andrew <achcdp@mst.edu>
2018-10-18 22:09:31 +0000

    Updated server IP

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const hostname = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+const hostname = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 const port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 const server = http.createServer((req, res) => {
