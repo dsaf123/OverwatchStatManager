8c0453ad73b752fb0ed6de44ffbed5a0ca6e9730
Andrew <achcdp@mst.edu>
2018-10-18 22:14:31 +0000

    Updated more things

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const hostname = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-const port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const hostname = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
