73a87353ab9a2e3853d277da4b41236e3849a293
Andrew <achcdp@mst.edu>
2018-10-19 11:21:36 +0000

    Fixed port issue

---
+++ .next/static/NuKd7eKyhzrjjKUpd4wz6/pages/about.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{203:function(e,n,t){__NEXT_REGISTER_PAGE("/about",function(){return e.exports=t(204),{page:e.exports.default}})},204:function(e,n,t){"use strict";t.r(n);var u=t(1),a=t.n(u);n.default=function(){return a.a.createElement("div",null,a.a.createElement("p",null,"This is the about page"))}}},[[203,1,0]]]);
--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5c1c9efabd9752b2ec82";
+/******/ 	var hotCurrentHash = "e60d495d57b698b51de3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
