763cfbbc7e5a17c898ec81275d289d9a4aba08db
Andrew <achcdp@mst.edu>
2018-11-16 10:29:00 +0000

    Updated index

--- dbapp/index.js
+++ dbapp/index.js
@@ -92,7 +92,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '../../webapp/public/index.html'))
 })
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3001
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 app.listen(port, "0.0.0.0", () => {
   console.log("Server listening on " + port);
 });
